c9f91558c32da854b53af8d9432af86011f1124f
Shankhya Subhra Datta <shankhya.s.datta@indusnet.co.in>
2023-05-29 15:57:42 +0530

    initial commit

---
+++ controller.js
@@ -0,0 +1,127 @@
+export const noticeData = async (req, res, next) => {
+  try {
+    let noticeIds = null;
+
+    if (req.body.ids.length > 0) {
+      noticeIds = await noticeService.downloadNotice(req.body.ids);
+    }
+
+    if (noticeIds) {
+      let data = [];
+
+      data = noticeIds.map((record) => {
+        return {
+          id: record.notices_id,
+          범주: record.category,
+          제목: record.title,
+          설명: record.description,
+          파일_이름: record.file_name,
+          파일_원본_이름: record.file_original_name,
+          데이트: record.update_date,
+        };
+      });
+
+      const PATH = "src//public//noticeData.xlsx";
+      const spreadsheet = new SpreadSheet(PATH);
+      spreadsheet.exportDataFile(data);
+
+      res.status(200).download(PATH);
+    } else {
+      const title = req.query.title ? req.query.name : "";
+
+      let data = [];
+
+      const listData = await noticeService.noticeSearch(title);
+
+      data = listData.map((record) => {
+        return {
+          id: record.notices_id,
+          범주: record.category,
+          제목: record.title,
+          설명: record.description,
+          파일_이름: record.file_name,
+          파일_원본_이름: record.file_original_name,
+          데이트: record.update_date,
+        };
+      });
+
+      const PATH = "src//public//noticeData.xlsx";
+      const spreadsheet = new SpreadSheet(PATH);
+      spreadsheet.exportDataFile(data);
+
+      res.status(200).download(PATH);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const inquiryData = async (req, res, next) => {
+    try {
+      let inquiryIds = null;
+      if (req.body.inquiryIds.length > 0) {
+        inquiryIds = await inquiryService.downloadInquiry(req.body.inquiryIds);
+      }
+      if (inquiryIds) {
+        let listData = [];
+        listData = inquiryIds.map((item) => {
+          return {
+            인덱스: item.id,
+            제목: item.title,
+            문의_작성_인덱스: item.question_user_id,
+            문의_작성_이름: decryptData(item.user_name),
+            문의_작성_회사_유형: USER_COMPANY_STATUS[item.company_classification],
+            문의_작성_회사_이름: item.company_name,
+            답변_상태: item.status,
+            문의_날짜: item.create_date,
+            전화_번호: decryptData(item.phone_number),
+          };
+        });
+  
+        const PATH = "src//public//inquiryData.xlsx";
+        const spreadsheet = new SpreadSheet(PATH);
+        spreadsheet.exportDataFile(listData);
+  
+        res.status(200).download(PATH);
+      } else {
+        const title = req.query.title;
+        const status = req.query.status;
+  
+        let inquiries = [];
+        const filters = {
+          title,
+          status,
+        }
+       
+        const listData = await inquiryService.searchInquiry({
+           ...filters
+        }
+        );
+        inquiries = listData.map((item) => {
+          return {
+            인덱스: item.id,
+            제목: item.title,
+            문의_작성_인덱스: item.question_user_id,
+            문의_작성_이름: decryptData(item.user_name),
+            문의_작성_회사_유형: USER_COMPANY_STATUS[item.company_classification],
+            문의_작성_회사_이름: item.company_name,
+            답변_상태: item.status,
+            문의_날짜: item.create_date,
+            전화_번호: decryptData(item.phone_number),
+          };
+        });
+        
+        const PATH = "src//public//inquiryData.xlsx";
+        const spreadsheet = new SpreadSheet(PATH);
+        spreadsheet.exportDataFile(inquiries);
+  
+        res.status(200).download(PATH);
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+  
---
+++ utils.js
@@ -0,0 +1,159 @@
+const generateStorage = (dirName) => {
+	return multer.diskStorage({
+	  destination: function (req, file, cb) {
+		cb(null, `./uploads/${dirName}`);
+	  },
+	  filename: async function (req, file, cb) {
+		const today = new Date();
+		const yyyy = today.getFullYear();
+		let mm = today.getMonth() + 1; // Months start at 0!
+		let dd = today.getDate();
+		const pathToCheck =
+		  dirName + "-" + dd + "-" + mm + "-" + yyyy + "-" + v4() + path.extname(file.originalname);
+		cb(null, pathToCheck);
+	  },
+	});
+  };
+  
+  const generateFilter = (allowedFileSize, allowedFileTypes) => (req, file, callback) => {
+	const acceptableExtensions = allowedFileTypes;
+	const mime = file.mimetype;
+	if (!acceptableExtensions.includes(mime)) {
+	  return callback({ code: "LIMIT_FILE_TYPE" });
+	}
+  
+	const fileSize = parseInt(req.headers["content-length"]);
+	if (fileSize > allowedFileSize) {
+	  return callback({ code: "LIMIT_FILE_SIZE_50" });
+	}
+  
+	callback(null, true);
+  };
+  
+  export const uploadAttachment = multer({
+	storage: generateStorage(UPLOAD_DIRECTORIES.EDITOR),
+	// fileFilter,
+  });
+
+
+  const key = String(envs.encryptSecretKey);
+const iv = key.substring(0, 16);
+const keyBuffer = Buffer.from(key, "utf-8");
+const ivBuffer = Buffer.from(iv, "utf-8");
+
+const getAlgorithm = (keyBase) => {
+  let key = Buffer.from(keyBase, "base64");
+  switch (key.length) {
+    case 16:
+      return "aes-128-cbc";
+    case 32:
+      return "aes-256-cbc";
+  }
+  throw new Error("Invalid key length: " + key.length);
+};
+
+export const encryptData = (text) => {
+  if (!text) return text;
+  try {
+    const cipher = crypto.createCipheriv(getAlgorithm(keyBuffer), keyBuffer, ivBuffer);
+    let encrypted = cipher.update(text, "utf-8", "base64");
+    encrypted += cipher.final("base64");
+    return encrypted;
+  } catch (e) {
+    //return text;
+    return "";
+  }
+};
+
+export const decryptData = (text) => {
+  if (!text) return text;
+  try {
+    const decipher = crypto.createDecipheriv(getAlgorithm(keyBuffer), keyBuffer, ivBuffer);
+    let decrypted = decipher.update(text, "base64", "utf-8");
+    decrypted += decipher.final();
+    return decrypted;
+  } catch (e) {
+    //return text;
+    return "";
+  }
+};
+
+export const createLogFile = async (fileName, fileData) => {
+	let obj = {
+	  logs: [],
+	};
+	fs.readFile(fileName, "utf8", function readFileCallback(err, data) {
+	  if (err) {
+		obj.logs.push(fileData);
+		const json = JSON.stringify(obj);
+		fs.writeFile(fileName, json, "utf8", function (err) {
+		  if (err) {
+			console.log(err);
+		  }
+		});
+	  } else {
+		obj = JSON.parse(data); //now it an object
+		obj.logs.push(fileData); //add some data
+		const json = JSON.stringify(obj); //convert it back to json
+		fs.writeFile(fileName, json, "utf8", function (err) {
+		  if (err) {
+			console.log(err);
+		  }
+		});
+	  }
+	});
+  };
+
+  const getPath = (req, file, dir) => {
+	if (!file) {
+	  return null;
+	}
+  
+	return `${req.protocol}://${req.headers.host}/uploads/${dir}/${file}`;
+  };
+  
+  export const GET_FILE_URL = {
+	diagnosis: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.DIAGNOSIS);
+	},
+	smartFactory: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.SMART_FACTORY);
+	},
+	smartDevice: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.SMART_DEVICE);
+	},
+	supplierIntroduction: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.SUPPLIER_INTRODUCTION);
+	},
+	supplycompanyImage: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.COMPANY);
+	},
+  
+	bestcompanyImage: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.BEST_PRACTICE);
+	},
+	notice: (req, file) => {
+	  return getPath(req, file, UPLOAD_DIRECTORIES.NOTICE);
+	},
+	bestPractice: (req, file) => {
+	  if (!file) {
+		return null;
+	  }
+	  return `${req.protocol}/${req.headers.host}/uploads/${UPLOAD_DIRECTORIES.BEST_PRACTICE}/${file}`;
+	},
+  };
+  
+  // Download attachment file
+  const getFilePath = (req, fileName, originalFileName, dir) => {
+	if (!fileName) {
+	  return null;
+	}
+  
+	return `${req.protocol}://${req.headers.host}/api/v1/user/downloadattachment/${dir}/${fileName}/${originalFileName}`;
+  };
+  
+  export const GET_FILE_PATH = {
+	smartFactory: (req, fileName, originalFileName) => {
+	  return getFilePath(req, fileName, originalFileName, UPLOAD_DIRECTORIES.SMART_FACTORY);
+	},
+  };
